0960670b462da231ed94a3fc455bcdaddd7dd75f
Alexandre Fernandes de Oliveira <alexandrefernandes_1@hotmail.com>
2025-02-04 23:28:33 -0400

    fix: try tailwind

--- dev/scripts/entrypoint.sh
+++ dev/scripts/entrypoint.sh
@@ -15,7 +15,9 @@ if [ "$ENVIRONMENT" = "development" ]; then
     wait
 else
     echo "Starting in production mode..."
-    tailwindcss -i ./static/css/input.css -o ./static/css/style.min.css --minify
+    curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+    chmod +x tailwindcss-linux-x64
+    tailwindcss-linux-x64 -i ./static/css/input.css -o ./static/css/style.min.css --minify
     templ generate
     go build -ldflags "-X main.Environment=production" -o ./bin/app ./cmd/main.go
     ./bin/app
